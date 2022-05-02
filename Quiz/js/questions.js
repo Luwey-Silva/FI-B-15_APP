00748b201c67cfc834d68ff93444c34da3f30394
Luwey-Silva <99865326+Luwey-Silva@users.noreply.github.com>
2022-05-02 12:57:39 +0200

    Add files via upload

---
+++ Quiz/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Beim welche Jahr fand die Industrialisierung statt?",
+    answer: "1815 - 1880",
+    options: [
+      "1815 - 1881",
+      "1815 - 1880",
+      "1813 - 1880",
+      "1914 - 1980"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Wann war der Bismarcks Sozialgesetztgebung?",
+    answer: "1890 - 1918",
+    options: [
+      "1890 - 1918",
+      "1880 - 1922",
+      "1894 - 1913",
+      "1890 - 1918"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Wann war der Weimarer Republik?",
+    answer: "1918 - 1933",
+    options: [
+      "1918 - 1933",
+      "1917 - 1934",
+      "1911 - 1923",
+      "1923 - 1936"
+    ]
+  },
+//    {
+//    numb: 4,
+//    question: "What does SQL stand for?",
+//    answer: "Structured Query Language",
+//    options: [
+//      "Stylish Question Language",
+//      "Stylesheet Query Language",
+//      "Statement Question Language",
+//      "Structured Query Language"
+//    ]
+//  },
+//    {
+//    numb: 5,
+//    question: "What does XML stand for?",
+//    answer: "eXtensible Markup Language",
+//    options: [
+//      "eXtensible Markup Language",
+//      "eXecutable Multiple Language",
+//      "eXTra Multi-Program Language",
+//      "eXamine Multiple Language"
+//    ]
+//  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
